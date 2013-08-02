63a0fca955734e7adb6f4f501c0de9597f23db3b
Dmitry-Me <wipedout@yandex.ru>
2013-08-02 19:24:10 +0400

    Remove redundant tab characters.

--- src/main/java/com/seriouscompany/business/java/fizzbuzz/packagenamingpackage/impl/loop/LoopInitializer.java
+++ src/main/java/com/seriouscompany/business/java/fizzbuzz/packagenamingpackage/impl/loop/LoopInitializer.java
@@ -1,9 +1,9 @@
 package com.seriouscompany.business.java.fizzbuzz.packagenamingpackage.impl.loop;
 
 public class LoopInitializer {
-	
+
 	private static final int LOOP_INIT_VALUE = 1;
-	
+
 	public int getLoopInitializationPoint() {
 		return LoopInitializer.LOOP_INIT_VALUE;
 	}
