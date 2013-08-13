37050179e32e9d86aa484bf3bb68afaa242a9d0c
Dmitry-Me <wipedout@yandex.ru>
2013-08-13 17:52:23 +0400

    Make conversions and doubles comparisons explicit.

--- src/main/java/com/seriouscompany/business/java/fizzbuzz/packagenamingpackage/impl/math/arithmetics/IntegerDivider.java
+++ src/main/java/com/seriouscompany/business/java/fizzbuzz/packagenamingpackage/impl/math/arithmetics/IntegerDivider.java
@@ -2,6 +2,10 @@ package com.seriouscompany.business.java.fizzbuzz.packagenamingpackage.impl.math
 
 import com.seriouscompany.business.java.fizzbuzz.packagenamingpackage.impl.strategies.comparators.integercomparator.ThreeWayIntegerComparator;
 import com.seriouscompany.business.java.fizzbuzz.packagenamingpackage.impl.strategies.comparators.integercomparator.ThreeWayIntegerComparisonResult;
+import com.seriouscompany.business.java.fizzbuzz.packagenamingpackage.impl.strategies.comparators.doublecomparator.FirstIsSmallerThanSecondDoubleComparator;
+import com.seriouscompany.business.java.fizzbuzz.packagenamingpackage.impl.strategies.comparators.doublecomparator.FirstIsLargerThanSecondDoubleComparator;
+import com.seriouscompany.business.java.fizzbuzz.packagenamingpackage.impl.strategies.converters.primitivetypesconverters.DoubleToIntConverter;
+import com.seriouscompany.business.java.fizzbuzz.packagenamingpackage.impl.strategies.converters.primitivetypesconverters.IntToDoubleConverter;
 
 public class IntegerDivider {
 	
@@ -13,16 +17,16 @@ public class IntegerDivider {
 		if(denominatorEqualsZeroResult == ThreeWayIntegerComparisonResult.FirstEqualsSecond){
 			throw new ArithmeticException("An attempt was made to divide by zero.");
 		}else{
-			double dbFirstNumber = (double) nFirstInteger;
-			double dbSecondNumber = (double) nSecondInteger;
+			double dbFirstNumber = IntToDoubleConverter.Convert(nFirstInteger);
+			double dbSecondNumber = IntToDoubleConverter.Convert(nSecondInteger);
 			double dbQuotient = dbFirstNumber / dbSecondNumber;
 			double dbRoundedQuotient = 0;
-			if(dbQuotient < 0){
+			if(FirstIsSmallerThanSecondDoubleComparator.FirstIsSmallerThanSecond(dbQuotient, 0)){
 				dbRoundedQuotient = Math.ceil(dbQuotient);
-			else if(dbQuotient > 0){
+			else if(FirstIsLargerThanSecondDoubleComparator.FirstIsLargerThanSecond(dbQuotient, 0)){
 				dbRoundedQuotient = Math.floor(dbQuotient);
 			}
-			int nIntegerQuotient = (int) dbRoundedQuotient;
+			int nIntegerQuotient = DoubleToIntConverter.Convert(dbRoundedQuotient);
 			return nIntegerQuotient;
 		}
 	}
---
+++ src/main/java/com/seriouscompany/business/java/fizzbuzz/packagenamingpackage/impl/strategies/converters/primitivetypesconverters/IntToDoubleConverter.java
@@ -0,0 +1,8 @@
+package com.seriouscompany.business.java.fizzbuzz.packagenamingpackage.impl.strategies.converters.primitivetypesconverters;
+
+public class IntToDoubleConverter {
+	public static double Convert(int nIntegerToConvert) {
+		double dbConversionResult = (double) nIntegerToConvert;
+		return dbConversionResult;
+	}
+}
