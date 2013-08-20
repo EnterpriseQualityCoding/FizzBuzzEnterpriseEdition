34637f456c6084447a1b91bca41cbbe4c178310c
Dmitry-Me <wipedout@yandex.ru>
2013-08-20 13:24:43 +0400

    Loop implementation factored out into a separate class.

--- src/main/java/com/seriouscompany/business/java/fizzbuzz/packagenamingpackage/impl/strategies/EnterpriseGradeFizzBuzzSolutionStrategy.java
+++ src/main/java/com/seriouscompany/business/java/fizzbuzz/packagenamingpackage/impl/strategies/EnterpriseGradeFizzBuzzSolutionStrategy.java
@@ -1,26 +1,18 @@
 package com.seriouscompany.business.java.fizzbuzz.packagenamingpackage.impl.strategies;
 
 import com.seriouscompany.business.java.fizzbuzz.packagenamingpackage.interfaces.strategies.FizzBuzzSolutionStrategy;
-import com.seriouscompany.business.java.fizzbuzz.packagenamingpackage.impl.factories.LoopComponentFactory;
-import com.seriouscompany.business.java.fizzbuzz.packagenamingpackage.impl.loop.LoopCondition;
-import com.seriouscompany.business.java.fizzbuzz.packagenamingpackage.impl.loop.LoopInitializer;
-import com.seriouscompany.business.java.fizzbuzz.packagenamingpackage.impl.loop.LoopStep;
+import com.seriouscompany.business.java.fizzbuzz.packagenamingpackage.impl.loop.LoopContext;
 
 public class EnterpriseGradeFizzBuzzSolutionStrategy implements FizzBuzzSolutionStrategy {
 
 	@Override
 	public void runSolution(int nFizzBuzzUpperLimit) {
-		LoopComponentFactory myLoopComponentFactory = new LoopComponentFactory();
-		LoopInitializer myLoopInitializer = myLoopComponentFactory.createLoopInitializer();
-		LoopFinalizer myLoopFinalizer = myLoopComponentFactory.createLoopFinalizer(nFizzBuzzUpperLimit);
-		LoopCondition myLoopCondition = myLoopComponentFactory.createLoopCondition();
-		LoopStep myLoopStep = myLoopComponentFactory.createLoopStep();
-
+		LoopContext loopContext = new LoopContext(nFizzBuzzUpperLimit);
 		SingleStepOutputGenerationStrategy myGenerationStrategy =
 			new SingleStepOutputGenerationStrategy();
 
-		for (int nCurrentNumber = myLoopInitializer.getLoopInitializationPoint(); myLoopCondition.evaluateLoop(nCurrentNumber, myLoopFinalizer.getLoopFinalizationPoint()); nCurrentNumber = myLoopStep.stepLoop(nCurrentNumber)) {
-			myGenerationStrategy.performGenerationForCurrentStep(nCurrentNumber);
+		for (loopContext.start(); loopContext.shouldProceed(); loopContext.proceed()) {
+			myGenerationStrategy.performGenerationForCurrentStep(loopContext.getControlParameter());
 		}
 	}
 }
