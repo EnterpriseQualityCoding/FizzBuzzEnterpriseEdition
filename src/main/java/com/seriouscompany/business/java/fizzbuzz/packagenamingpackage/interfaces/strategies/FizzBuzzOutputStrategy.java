779b3b49b1819e4da470f1008dbfc6fa67d55313
bstempi <brian.stempin@gmail.com>
2014-03-04 18:02:15 -0500

    Adding a much needed output strategy to get away from that old, crusty, System.out construct

---
+++ src/main/java/com/seriouscompany/business/java/fizzbuzz/packagenamingpackage/interfaces/strategies/FizzBuzzOutputStrategy.java
@@ -0,0 +1,7 @@
+package com.seriouscompany.business.java.fizzbuzz.packagenamingpackage.interfaces.strategies;
+
+public interface FizzBuzzOutputStrategy {
+	
+	public void output(String output);
+	
+}
