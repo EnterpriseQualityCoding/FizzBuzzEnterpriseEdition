8855d432005f80f9de673003beb74593b628b6b3
Wiktor Mociun <voter101@gmail.com>
2014-03-25 11:11:54 +0100

    Better naming convention

--- src/main/java/com/seriouscompany/business/java/fizzbuzz/packagenamingpackage/impl/Main.java
+++ src/main/java/com/seriouscompany/business/java/fizzbuzz/packagenamingpackage/impl/Main.java
@@ -3,6 +3,6 @@ package com.seriouscompany.business.java.fizzbuzz.packagenamingpackage.impl;
 public class Main {
 	public static void main(String[] args) {
 		final FizzBuzz myFizzBuzz = new FizzBuzz();
-		myFizzBuzz.fizzbuzz(100);
+		myFizzBuzz.fizzBuzz(100);
 	}
 }
