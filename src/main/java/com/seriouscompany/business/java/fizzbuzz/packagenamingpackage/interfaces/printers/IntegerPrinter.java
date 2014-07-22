aad80defc4e495d0c13a3e180b293b51e3c860d5
Dmitry-Me <wipedout@yandex.ru>
2014-07-22 13:53:57 +0400

    Merge https://github.com/EnterpriseQualityCoding/FizzBuzzEnterpriseEdition/pull/127

--- src/main/java/com/seriouscompany/business/java/fizzbuzz/packagenamingpackage/interfaces/printers/IntegerPrinter.java
+++ src/main/java/com/seriouscompany/business/java/fizzbuzz/packagenamingpackage/interfaces/printers/IntegerPrinter.java
@@ -1,6 +1,6 @@
 package com.seriouscompany.business.java.fizzbuzz.packagenamingpackage.interfaces.printers;
 
-public interface IntegerPrinter {
+public interface IntegerPrinter extends DataPrinter {
 
 	public void printInteger(int theInteger);
 
