dc68cf6459dac4fc117b4fb2d6fb1c91a53ee3be
Dmitry-Me <wipedout@yandex.ru>
2014-08-07 11:04:51 +0400

    Use mechanism for obtaining the upper limit value.

---
+++ src/main/java/com/seriouscompany/business/java/fizzbuzz/packagenamingpackage/interfaces/parameters/FizzBuzzUpperLimitParameter.java
@@ -0,0 +1,7 @@
+package com.seriouscompany.business.java.fizzbuzz.packagenamingpackage.interfaces.parameters;
+
+public interface FizzBuzzUpperLimitParameter {
+
+	public int ObtainUpperLimitValue();
+}
+
