f12b2e7f18b1c2d8bfaff31bf64c1d3f90985a46
Adrian Chifor <chifor.adrian1993@gmail.com>
2015-01-20 16:34:43 +0000

    Replaced constant string values with final values

--- src/main/java/com/seriouscompany/business/java/fizzbuzz/packagenamingpackage/impl/stringreturners/NewLineStringReturner.java
+++ src/main/java/com/seriouscompany/business/java/fizzbuzz/packagenamingpackage/impl/stringreturners/NewLineStringReturner.java
@@ -3,11 +3,15 @@ package com.seriouscompany.business.java.fizzbuzz.packagenamingpackage.impl.stri
 import com.seriouscompany.business.java.fizzbuzz.packagenamingpackage.interfaces.stringreturners.StringStringReturner;
 
 public class NewLineStringReturner implements StringStringReturner {
+
+	public static final String LINE_SEPARATOR_STRING_VALUE = "line" + "." + "separator";
 	
 	public String getReturnString() {
-		final String systemDefaultNewLineString = System.getProperty("line.separator");
+		final String systemDefaultNewLineString = System.getProperty(SEPARATOR_STRING_VALUE);
 		final StringBuilder myStringBuilder = new StringBuilder(systemDefaultNewLineString);
 		final String myString = myStringBuilder.toString();
 		return new String(myString);
+
 	}
+
 }
