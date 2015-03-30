828d49921ab0a7ba7aff230ce191e2eb1656f947
Dmitry-Me <wipedout@yandex.ru>
2015-03-30 13:18:11 +0300

    Fix code style violation reported in PR#184

--- src/main/java/com/seriouscompany/business/java/fizzbuzz/packagenamingpackage/interfaces/parameters/FizzBuzzUpperLimitParameter.java
+++ src/main/java/com/seriouscompany/business/java/fizzbuzz/packagenamingpackage/interfaces/parameters/FizzBuzzUpperLimitParameter.java
@@ -2,6 +2,6 @@ package com.seriouscompany.business.java.fizzbuzz.packagenamingpackage.interface
 
 public interface FizzBuzzUpperLimitParameter {
 
-	public int ObtainUpperLimitValue();
+	public int obtainUpperLimitValue();
 }
 
