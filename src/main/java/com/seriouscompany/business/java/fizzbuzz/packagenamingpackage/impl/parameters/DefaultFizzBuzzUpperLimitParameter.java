0b95f1ed7afbc0be9ac9a052a61b60f3ba135ffa
Emily Curtin <emily@framebit.org>
2015-12-06 11:26:10 -0500

    Documentation for Parameters package

--- src/main/java/com/seriouscompany/business/java/fizzbuzz/packagenamingpackage/impl/parameters/DefaultFizzBuzzUpperLimitParameter.java
+++ src/main/java/com/seriouscompany/business/java/fizzbuzz/packagenamingpackage/impl/parameters/DefaultFizzBuzzUpperLimitParameter.java
@@ -8,14 +8,24 @@ public final class DefaultFizzBuzzUpperLimitParameter implements FizzBuzzUpperLi
 
 	private final int fizzBuzzUpperLimitParameterValue;
 
+	/**
+	 * @return
+	 */
 	public DefaultFizzBuzzUpperLimitParameter() {
 		this.fizzBuzzUpperLimitParameterValue = DefaultFizzBuzzUpperLimitParameter.DEFAULT_FIZZ_BUZZ_UPPER_LIMIT_PARAMETER_VALUE;
 	}
 
+	/**
+	 * @param fizzBuzzUpperLimitParameterValue
+	 * @return
+     */
 	public DefaultFizzBuzzUpperLimitParameter(final int fizzBuzzUpperLimitParameterValue) {
 		this.fizzBuzzUpperLimitParameterValue = fizzBuzzUpperLimitParameterValue;
 	}
 
+	/**
+	 * @return
+     */
 	public int obtainUpperLimitValue() {
 		return this.fizzBuzzUpperLimitParameterValue;
 	}
