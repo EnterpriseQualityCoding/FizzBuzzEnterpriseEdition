e3a1d2d983a793d804a888ac4c4ef23597a21315
Vojtech Krasa <vojta.krasa@gmail.com>
2016-07-26 12:49:08 +0200

    Renamed some constants to remove ambiguity

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -45,22 +45,23 @@ public class FizzBuzzTest {
 
 	@Test
 	public void testFizzBuzz() throws IOException {
-		this.doFizzBuzz(TestConstants._1, TestConstants._1_);
-		this.doFizzBuzz(TestConstants._2, TestConstants._1_2_);
-		this.doFizzBuzz(TestConstants._3, TestConstants._1_2_FIZZ);
-		this.doFizzBuzz(TestConstants._4, TestConstants._1_2_FIZZ_4);
-		this.doFizzBuzz(TestConstants._5, TestConstants._1_2_FIZZ_4_BUZZ);
-		this.doFizzBuzz(TestConstants._6, TestConstants._1_2_FIZZ_4_BUZZ_FIZZ);
-		this.doFizzBuzz(TestConstants._7, TestConstants._1_2_FIZZ_4_BUZZ_FIZZ_7);
-		this.doFizzBuzz(TestConstants._8, TestConstants._1_2_FIZZ_4_BUZZ_FIZZ_7_8);
-		this.doFizzBuzz(TestConstants._9, TestConstants._1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ);
-		this.doFizzBuzz(TestConstants._10, TestConstants._1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ);
-		this.doFizzBuzz(TestConstants._11, TestConstants._1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11);
-		this.doFizzBuzz(TestConstants._12, TestConstants._1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11_FIZZ);
-		this.doFizzBuzz(TestConstants._13, TestConstants._1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11_FIZZ_13);
-		this.doFizzBuzz(TestConstants._14, TestConstants._1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11_FIZZ_13_14);
-		this.doFizzBuzz(TestConstants._15, TestConstants._1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11_FIZZ_13_14_FIZZ_BUZZ);
-		this.doFizzBuzz(TestConstants._16,
+		this.doFizzBuzz(TestConstants.INT_1, TestConstants._1_);
+		this.doFizzBuzz(TestConstants.INT_2, TestConstants._1_2_);
+		this.doFizzBuzz(TestConstants.INT_3, TestConstants._1_2_FIZZ);
+		this.doFizzBuzz(TestConstants.INT_4, TestConstants._1_2_FIZZ_4);
+		this.doFizzBuzz(TestConstants.INT_5, TestConstants._1_2_FIZZ_4_BUZZ);
+		this.doFizzBuzz(TestConstants.INT_6, TestConstants._1_2_FIZZ_4_BUZZ_FIZZ);
+		this.doFizzBuzz(TestConstants.INT_7, TestConstants._1_2_FIZZ_4_BUZZ_FIZZ_7);
+		this.doFizzBuzz(TestConstants.INT_8, TestConstants._1_2_FIZZ_4_BUZZ_FIZZ_7_8);
+		this.doFizzBuzz(TestConstants.INT_9, TestConstants._1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ);
+		this.doFizzBuzz(TestConstants.INT_10, TestConstants._1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ);
+		this.doFizzBuzz(TestConstants.INT_11, TestConstants._1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11);
+		this.doFizzBuzz(TestConstants.INT_12, TestConstants._1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11_FIZZ);
+		this.doFizzBuzz(TestConstants.INT_13, TestConstants._1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11_FIZZ_13);
+		this.doFizzBuzz(TestConstants.INT_14, TestConstants._1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11_FIZZ_13_14);
+		this.doFizzBuzz(TestConstants.INT_15,
+				TestConstants._1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11_FIZZ_13_14_FIZZ_BUZZ);
+		this.doFizzBuzz(TestConstants.INT_16,
 				TestConstants._1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11_FIZZ_13_14_FIZZ_BUZZ_16);
 	}
 }
