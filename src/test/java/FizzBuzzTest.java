fa2ba3712a0e45a77fe87db07f4930a68ee17f41
Vojtech Krasa <vojta.krasa@gmail.com>
2016-07-26 12:44:56 +0200

    Introduced TestConstants

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -16,31 +16,13 @@ import com.seriouscompany.business.java.fizzbuzz.packagenamingpackage.interfaces
 
 public class FizzBuzzTest {
 
-	private static final String _1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11_FIZZ_13_14_FIZZ_BUZZ_16 = "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz\n16\n";
-	private static final String _1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11_FIZZ_13_14_FIZZ_BUZZ = "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz\n";
-	private static final String _1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11_FIZZ_13_14 = "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\n";
-	private static final String _1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11_FIZZ_13 = "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n";
-	private static final String _1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11_FIZZ = "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n";
-	private static final String _1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11 = "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\n";
-	private static final String _1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ = "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n";
-	private static final String _1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ = "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\n";
-	private static final String _1_2_FIZZ_4_BUZZ_FIZZ_7_8 = "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\n";
-	private static final String _1_2_FIZZ_4_BUZZ_FIZZ_7 = "1\n2\nFizz\n4\nBuzz\nFizz\n7\n";
-	private static final String _1_2_FIZZ_4_BUZZ_FIZZ = "1\n2\nFizz\n4\nBuzz\nFizz\n";
-	private static final String _1_2_FIZZ_4_BUZZ = "1\n2\nFizz\n4\nBuzz\n";
-	private static final String _1_2_FIZZ_4 = "1\n2\nFizz\n4\n";
-	private static final String _1_2_FIZZ = "1\n2\nFizz\n";
-	private static final String _1_2_ = "1\n2\n";
-	private static final String _1_ = "1\n";
-	private static final String STANDARD_FIZZ_BUZZ = "standardFizzBuzz";
-	private static final String SPRING_XML = "spring.xml";
 	private PrintStream out;
 	private FizzBuzz fb;
 
 	@Before
 	public void setUp() {
-		final ApplicationContext context = new ClassPathXmlApplicationContext(SPRING_XML);
-		this.fb = (FizzBuzz) context.getBean(STANDARD_FIZZ_BUZZ);
+		final ApplicationContext context = new ClassPathXmlApplicationContext(TestConstants.SPRING_XML);
+		this.fb = (FizzBuzz) context.getBean(TestConstants.STANDARD_FIZZ_BUZZ);
 		this.out = System.out;
 		((ConfigurableApplicationContext) context).close();
 	}
@@ -63,21 +45,22 @@ public class FizzBuzzTest {
 
 	@Test
 	public void testFizzBuzz() throws IOException {
-		this.doFizzBuzz(1, _1_);
-		this.doFizzBuzz(2, _1_2_);
-		this.doFizzBuzz(3, _1_2_FIZZ);
-		this.doFizzBuzz(4, _1_2_FIZZ_4);
-		this.doFizzBuzz(5, _1_2_FIZZ_4_BUZZ);
-		this.doFizzBuzz(6, _1_2_FIZZ_4_BUZZ_FIZZ);
-		this.doFizzBuzz(7, _1_2_FIZZ_4_BUZZ_FIZZ_7);
-		this.doFizzBuzz(8, _1_2_FIZZ_4_BUZZ_FIZZ_7_8);
-		this.doFizzBuzz(9, _1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ);
-		this.doFizzBuzz(10, _1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ);
-		this.doFizzBuzz(11, _1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11);
-		this.doFizzBuzz(12, _1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11_FIZZ);
-		this.doFizzBuzz(13, _1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11_FIZZ_13);
-		this.doFizzBuzz(14, _1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11_FIZZ_13_14);
-		this.doFizzBuzz(15, _1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11_FIZZ_13_14_FIZZ_BUZZ);
-		this.doFizzBuzz(16, _1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11_FIZZ_13_14_FIZZ_BUZZ_16);
+		this.doFizzBuzz(TestConstants._1, TestConstants._1_);
+		this.doFizzBuzz(TestConstants._2, TestConstants._1_2_);
+		this.doFizzBuzz(TestConstants._3, TestConstants._1_2_FIZZ);
+		this.doFizzBuzz(TestConstants._4, TestConstants._1_2_FIZZ_4);
+		this.doFizzBuzz(TestConstants._5, TestConstants._1_2_FIZZ_4_BUZZ);
+		this.doFizzBuzz(TestConstants._6, TestConstants._1_2_FIZZ_4_BUZZ_FIZZ);
+		this.doFizzBuzz(TestConstants._7, TestConstants._1_2_FIZZ_4_BUZZ_FIZZ_7);
+		this.doFizzBuzz(TestConstants._8, TestConstants._1_2_FIZZ_4_BUZZ_FIZZ_7_8);
+		this.doFizzBuzz(TestConstants._9, TestConstants._1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ);
+		this.doFizzBuzz(TestConstants._10, TestConstants._1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ);
+		this.doFizzBuzz(TestConstants._11, TestConstants._1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11);
+		this.doFizzBuzz(TestConstants._12, TestConstants._1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11_FIZZ);
+		this.doFizzBuzz(TestConstants._13, TestConstants._1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11_FIZZ_13);
+		this.doFizzBuzz(TestConstants._14, TestConstants._1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11_FIZZ_13_14);
+		this.doFizzBuzz(TestConstants._15, TestConstants._1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11_FIZZ_13_14_FIZZ_BUZZ);
+		this.doFizzBuzz(TestConstants._16,
+				TestConstants._1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11_FIZZ_13_14_FIZZ_BUZZ_16);
 	}
 }
---
+++ src/test/java/TestConstants.java
@@ -0,0 +1,41 @@
+public class TestConstants {
+
+	private TestConstants() {
+		super();
+	}
+
+	static final String _1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11_FIZZ_13_14_FIZZ_BUZZ_16 = "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz\n16\n";
+	static final String _1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11_FIZZ_13_14_FIZZ_BUZZ = "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz\n";
+	static final String _1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11_FIZZ_13_14 = "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\n";
+	static final String _1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11_FIZZ_13 = "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n";
+	static final String _1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11_FIZZ = "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n";
+	static final String _1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11 = "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\n";
+	static final String _1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ = "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n";
+	static final String _1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ = "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\n";
+	static final String _1_2_FIZZ_4_BUZZ_FIZZ_7_8 = "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\n";
+	static final String _1_2_FIZZ_4_BUZZ_FIZZ_7 = "1\n2\nFizz\n4\nBuzz\nFizz\n7\n";
+	static final String _1_2_FIZZ_4_BUZZ_FIZZ = "1\n2\nFizz\n4\nBuzz\nFizz\n";
+	static final String _1_2_FIZZ_4_BUZZ = "1\n2\nFizz\n4\nBuzz\n";
+	static final String _1_2_FIZZ_4 = "1\n2\nFizz\n4\n";
+	static final String _1_2_FIZZ = "1\n2\nFizz\n";
+	static final String _1_2_ = "1\n2\n";
+	static final String _1_ = "1\n";
+	static final String STANDARD_FIZZ_BUZZ = "standardFizzBuzz";
+	static final String SPRING_XML = "spring.xml";
+	static final int _1 = 1;
+	static final int _2 = 2;
+	static final int _3 = 3;
+	static final int _4 = 4;
+	static final int _5 = 5;
+	static final int _6 = 6;
+	static final int _7 = 7;
+	static final int _8 = 8;
+	static final int _9 = 9;
+	static final int _10 = 10;
+	static final int _11 = 11;
+	static final int _12 = 12;
+	static final int _13 = 13;
+	static final int _14 = 14;
+	static final int _15 = 15;
+	static final int _16 = 16;
+}
