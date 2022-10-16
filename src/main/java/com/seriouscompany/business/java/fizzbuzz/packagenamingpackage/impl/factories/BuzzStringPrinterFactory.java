1dd19706523c340279dc12352fdf6bd6be1de570
samr <samar.thind@outlook.com>
2022-10-16 11:26:01 -0700

    Factories

--- src/main/java/com/seriouscompany/business/java/fizzbuzz/packagenamingpackage/impl/factories/BuzzStringPrinterFactory.java
+++ src/main/java/com/seriouscompany/business/java/fizzbuzz/packagenamingpackage/impl/factories/BuzzStringPrinterFactory.java
@@ -1,10 +1,19 @@
+// BuzzStringPrinterFactory.java
+// Faactory for BuzzStringPrinter
+
+// add to package com.seriouscompany.business.java.fizzbuzz.packagenamingpackage.impl.factories
 package com.seriouscompany.business.java.fizzbuzz.packagenamingpackage.impl.factories;
 
+// imports org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Autowired;
+// imports org.springframework.stereotype.Service
 import org.springframework.stereotype.Service;
 
+// imports com.seriouscompany.business.java.fizzbuzz.packagenamingpackage.impl.printers.BuzzStringPrinter
 import com.seriouscompany.business.java.fizzbuzz.packagenamingpackage.impl.printers.BuzzStringPrinter;
+// imports com.seriouscompany.business.java.fizzbuzz.packagenamingpackage.interfaces.factories.StringPrinterFactory
 import com.seriouscompany.business.java.fizzbuzz.packagenamingpackage.interfaces.factories.StringPrinterFactory;
+// imports com.seriouscompany.business.java.fizzbuzz.packagenamingpackage.interfaces.printers.StringPrinter
 import com.seriouscompany.business.java.fizzbuzz.packagenamingpackage.interfaces.printers.StringPrinter;
 
 /**
@@ -13,14 +22,18 @@ import com.seriouscompany.business.java.fizzbuzz.packagenamingpackage.interfaces
 @Service
 public class BuzzStringPrinterFactory implements StringPrinterFactory {
 
+	// create a private constant using BuzzStringPrinter called `_buzzStringPrinter` with no value
 	private final BuzzStringPrinter _buzzStringPrinter;
 
 	/**
 	 * @param _buzzStringPrinter BuzzStringPrinter
 	 */
 	@Autowired
+	// create constructor
 	public BuzzStringPrinterFactory(final BuzzStringPrinter _buzzStringPrinter) {
+		// create a quick pointer `super` thing to access the accessor from the accessor method of the accessed object
 		super();
+		// set attribute `_buzzStringPrinter` to parameter constant `_buzzStringParameter`
 		this._buzzStringPrinter = _buzzStringPrinter;
 	}
 
@@ -28,7 +41,9 @@ public class BuzzStringPrinterFactory implements StringPrinterFactory {
 	 * @return StringPrinter
 	 */
 	@Override
+	// create public method called `createStringPrinter` which returns type StringPrinter
 	public StringPrinter createStringPrinter() {
+		// return attribute `_buzzStringPrinter`
 		return this._buzzStringPrinter;
 	}
 
