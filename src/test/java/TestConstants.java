150eb6e3823e98a7621e6f31b275a0fd2ced9af8
samr <samr.stuff@pm.me>
2023-05-15 23:27:39 +0000

    test.java.TestConstants

--- src/test/java/TestConstants.java
+++ src/test/java/TestConstants.java
@@ -1,45 +1,81 @@
+// Constants.java
+// Costants for FizzBuzz tests
+
 /**
  * Constants for FizzBuzz tests
  */
 public class TestConstants {
-
+	// create a private constructor for the class TestConstants defined on line 7 which calls itself
 	private TestConstants() {
 		super();
 	}
 
+	// a public, static constant property of type String called _1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11_FIZZ_13_14_FIZZ_BUZZ_16 which contains the text "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz\n16\n"
 	static final String _1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11_FIZZ_13_14_FIZZ_BUZZ_16 = "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz\n16\n";
+	// a public, static constant property of type String called _1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11_FIZZ_13_14_FIZZ_BUZZ which contains the text "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz\n"
 	static final String _1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11_FIZZ_13_14_FIZZ_BUZZ = "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz\n";
+	// a public, static constant property of type String called _1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11_FIZZ_13_14 which contains the text "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\n"
 	static final String _1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11_FIZZ_13_14 = "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\n";
+	// a public, static constant property of type String called _1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11_FIZZ_13 which contains the text "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n"
 	static final String _1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11_FIZZ_13 = "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n";
+	// a public, static constant property of type String called _1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11_FIZZ which contains the text "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n"
 	static final String _1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11_FIZZ = "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n";
+	// a public, static constant property of type String called _1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11 which contains the text "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\n"
 	static final String _1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ_11 = "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\n";
+	// a public, static constant property of type String called _1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ which contains the text "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n"
 	static final String _1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ_BUZZ = "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n";
+	// a public, static constant property of type String called _1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ which contains the text "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\n"
 	static final String _1_2_FIZZ_4_BUZZ_FIZZ_7_8_FIZZ = "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\n";
+	// a public, static constant property of type String called _1_2_FIZZ_4_BUZZ_FIZZ_7_8 which contains the text "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\n"
 	static final String _1_2_FIZZ_4_BUZZ_FIZZ_7_8 = "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\n";
+	// a public, static constant property of type String called _1_2_FIZZ_4_BUZZ_FIZZ_7 which contains the text "1\n2\nFizz\n4\nBuzz\nFizz\n7\n"
 	static final String _1_2_FIZZ_4_BUZZ_FIZZ_7 = "1\n2\nFizz\n4\nBuzz\nFizz\n7\n";
+	// a public, static constant property of type String called _1_2_FIZZ_4_BUZZ_FIZZ which contains the text "1\n2\nFizz\n4\nBuzz\nFizz\n"
 	static final String _1_2_FIZZ_4_BUZZ_FIZZ = "1\n2\nFizz\n4\nBuzz\nFizz\n";
+	// a public, static constant property of type String called _1_2_FIZZ_4_BUZZ which contains the text "1\n2\nFizz\n4\nBuzz\n"
 	static final String _1_2_FIZZ_4_BUZZ = "1\n2\nFizz\n4\nBuzz\n";
+	// a public, static constant property of type String called _1_2_FIZZ_4 which contains the text "1\n2\nFizz\n4\n"
 	static final String _1_2_FIZZ_4 = "1\n2\nFizz\n4\n";
+	// a public, static constant property of type String called _1_2_FIZZ which contains the text "1\n2\nFizz\n"
 	static final String _1_2_FIZZ = "1\n2\nFizz\n";
+	// a public, static constant property of type String called _1_2_ which contains the text "1\n2\n"
 	static final String _1_2_ = "1\n2\n";
+	// a public, static constant property of type String called _1_ which contains the text "1\n"
 	static final String _1_ = "1\n";
+	// a public, static constant property of type String called STANDARD_FIZZ_BUZZ which contains the text "standardFizzBuzz"
 	static final String STANDARD_FIZZ_BUZZ = "standardFizzBuzz";
+	// a public, static constant property of type String called SPRING_XML which contains the text "spring.xml"
 	static final String SPRING_XML = "spring.xml";
+	// a public, static constant property of type int called INT_1 which contains the number 1
 	static final int INT_1 = 1;
+	// a public, static constant property of type int called INT_2 which contains the number 2
 	static final int INT_2 = 2;
+	// a public, static constant property of type int called INT_3 which contains the number 3
 	static final int INT_3 = 3;
+	// a public, static constant property of type int called INT_4 which contains the number 4
 	static final int INT_4 = 4;
+	// a public, static constant property of type int called INT_5 which contains the number 5
 	static final int INT_5 = 5;
+	// a public, static constant property of type int called INT_6 which contains the number 6
 	static final int INT_6 = 6;
+	// a public, static constant property of type int called INT_7 which contains the number 7
 	static final int INT_7 = 7;
+	// a public, static constant property of type int called INT_8 which contains the number 8
 	static final int INT_8 = 8;
+	// a public, static constant property of type int called INT_9 which contains the number 9
 	static final int INT_9 = 9;
+	// a public, static constant property of type int called INT_10 which contains the number 10
 	static final int INT_10 = 10;
+	// a public, static constant property of type int called INT_11 which contains the number 11
 	static final int INT_11 = 11;
+	// a public, static constant property of type int called INT_12 which contains the number 12
 	static final int INT_12 = 12;
+	// a public, static constant property of type int called INT_13 which contains the number 13
 	static final int INT_13 = 13;
+	// a public, static constant property of type int called INT_14 which contains the number 14
 	static final int INT_14 = 14;
+	// a public, static constant property of type int called INT_15 which contains the number 15
 	static final int INT_15 = 15;
+	// a public, static constant property of type int called INT_16 which contains the number 16
 	static final int INT_16 = 16;
-
 }
